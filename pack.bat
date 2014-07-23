268478499beeb640a4afdee4ae273cb28319b2a6
Marcelle von Wendland <mvw@hivves.com>
2014-07-23 16:41:53 +0100

    start2

---
+++ pack.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Marcelle\git\lab\clojure\ClUtils\target\test-classes\
+jar cvf C:\Users\Marcelle\git\lab\clojure\ClUtils\target\TestClasses.jar mikera\cljutils\* 
+cd C:\Users\Marcelle\git\lab\clojure\ClUtils\

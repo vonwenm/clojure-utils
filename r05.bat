177fa112872171f5e7350b8abf8e1a102b63adbc
Marcelle von Wendland <mvw@hivves.com>
2014-07-23 17:22:34 +0100

    start04

--- r05.bat
+++
@@ -1,1 +0,0 @@
-java -cp C:\Users\Marcelle\git\lab\clojure\ClUtils\target\clojure-utils-0.5.0.jar;C:\Users\Marcelle\git\lab\clojure\ClUtils\target\TestClasses.jar;C:\Users\Marcelle\git\lab\clojure\ClUtils\target\clojure-1.6.0.jar  mikera.cljutils.DemoApp
